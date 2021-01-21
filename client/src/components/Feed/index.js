584f67adb0de6a4fc5667050648b3a381730d9b5
Mitch <mitch.mathias96@gmail.com>
2021-01-21 14:19:42 -0500

    feed

--- client/src/components/Feed/index.js
+++ client/src/components/Feed/index.js
@@ -1,10 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Jumbotron from '../Jumbotron'
+import { Col, Row, Container } from "../Grid";
+import API from '../../utils/API'
 import '../../style.css'
+import { List, ListItem } from "../List";
 
-function Feed () {
-    return (
-        <div></div>
-    )
+class Feed extends Component {
+
+    state = {
+        search: "",
+        events: [],
+        filtEvents: []
+    }
+
+    componentDidMount() {
+        this.getEvents()
+    }
+
+    handleInputChange = (e) => {
+        const title = e.target.title;
+        const value = e.target.value;
+        this.setState({ [title]: value }, function () {
+            const searched = this.state.search.toLowerCase()
+            const filteredEvents = this.state.events.filter(evs => {
+                let title = `${evs.title}`
+                title = title.toLowerCase()
+                return title.includes(searched)
+            })
+            this.setState({ filtEvents: filteredEvents })
+        })
+    }
+
+    keyPress = (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault()
+        }
+    }
+
+    getEvents = () => {
+        API.getEvents().then(res => {
+            console.log(res.data.results)
+            this.setState({ events: res.data.results, filtEvents: res.data.results })
+        })
+    }
+
+
+    render() {
+        return (
+            <Container fluid>
+                <Row>
+                    <Col size="md-6">
+                        <Jumbotron>
+                            <h1>events</h1>
+                        </Jumbotron>
+
+                    </Col>
+                    <Col size="md-6 sm-12">
+
+                            <List>
+                                    <ListItem filtEvents ={this.state.filtEvents}/>
+                            </List>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
 };
 
 export default Feed 
--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -2,84 +2,84 @@
 import React from "react";
 
 function Signup(){
-    const signUpForm = $("#signup-form");
-    const emailInput = $("input#email-input");
-    const passwordInput = $("input#password-input");
-    const clearanceInput = $("input#clearance-input");
+    // const signUpForm = $("#signup-form");
+    // const emailInput = $("input#email-input");
+    // const passwordInput = $("input#password-input");
+    // const clearanceInput = $("input#clearance-input");
   
-    // When the signup button is clicked, we validate the email and password are not blank
-    signUpForm.on("submit", event => {
-      event.preventDefault();
-      const userData = {
-        email: emailInput.val().trim(),
-        password: passwordInput.val().trim(),
-        clearance: clearanceInput.val().trim()
-      };
+    // // When the signup button is clicked, we validate the email and password are not blank
+    // signUpForm.on("submit", event => {
+    //   event.preventDefault();
+    //   const userData = {
+    //     email: emailInput.val().trim(),
+    //     password: passwordInput.val().trim(),
+    //     clearance: clearanceInput.val().trim()
+    //   };
   
-      if (!userData.email || !userData.password || !userData.clearance) {
-        return;
-      }
+    //   if (!userData.email || !userData.password || !userData.clearance) {
+    //     return;
+    //   }
   
-      // If we have an email and password, run the signUpUser function
-      signUpUser(userData.email, userData.password, userData.clearance);
-      emailInput.val("");
-      passwordInput.val("");
-      clearanceInput.val("");
-    });
+    //   // If we have an email and password, run the signUpUser function
+    //   signUpUser(userData.email, userData.password, userData.clearance);
+    //   emailInput.val("");
+    //   passwordInput.val("");
+    //   clearanceInput.val("");
+    // });
   
-    // Does a post to the signup route. If successful, we are redirected to the members page
-    // Otherwise we log any errors
-    function signUpUser(email, password, clearance) {
-      $.post("/api/signup", {
-        email: email,
-        password: password,
-        clearance: clearance
-      })
-        .then(() => {
-          window.location.replace("/members");
-          // If there's an error, handle it by throwing up a bootstrap alert
-        })
-        .catch(handleLoginErr);
-    }
+    // // Does a post to the signup route. If successful, we are redirected to the members page
+    // // Otherwise we log any errors
+    // function signUpUser(email, password, clearance) {
+    //   $.post("/api/signup", {
+    //     email: email,
+    //     password: password,
+    //     clearance: clearance
+    //   })
+    //     .then(() => {
+    //       window.location.replace("/members");
+    //       // If there's an error, handle it by throwing up a bootstrap alert
+    //     })
+    //     .catch(handleLoginErr);
+    // }
   
-    function handleLoginErr(err) {
-      $("#alert .msg").text(err.responseJSON);
-      $("#alert").fadeIn(500);
-    }
+    // function handleLoginErr(err) {
+    //   $("#alert .msg").text(err.responseJSON);
+    //   $("#alert").fadeIn(500);
+    // }
 
 return(
     <form id="signup-form">
 
-    <div class="form-group">
+    <div className="form-group">
       <label for="email">Email Address</label>
-      <input type="text" class="form-control" id="email-input" required />
+      <input type="text" className="form-control" id="email-input" required />
     </div>
-    <div class="form-group">
+    <div className="form-group">
       <label for="Password">Password</label>
       <input type="password" 
-      class="form-control" 
+      className="form-control" 
       id="password-input" 
       required value={this.state.value} 
       onChange={this.handleChange}/>
     </div>
 
-    <div class="form-group">
+    <div className="form-group">
       <label for="clearance">Clearance level</label>
       <input type="text" 
-      class="form-control" 
+      className="form-control" 
       id="clearance-input" 
       required value={this.state.value} 
       onChange={this.handleChange}/>
       <br />
     </div>
 
-    <div class="form-group">
-      <button type="submit" id="signUp" class="button is-light">Sign Up</button>
+    <div className="form-group">
+      <button type="submit" id="signUp" className="button is-light">Sign Up</button>
     </div>
     <br />
     <div>
-      <p class="subtitle">Already have an Account?</p>
-      <a class="button is-light" href="/login">Log In</a>
+      <p className="subtitle">Already have an Account?</p>
+      <a className="button is-light" href="/login">Log In</a>
     </div>
 
   </form>
