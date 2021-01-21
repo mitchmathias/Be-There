36c93069141c675662d395b2a35c11c5677b9b82
Omar Cossio <omar119mce@gmail.com>
2021-01-21 14:34:21 -0500

    skeleton of CreateEvent - title, Org Name, event name, location, date, and description

--- client/src/components/CreateEvent/index.js
+++ client/src/components/CreateEvent/index.js
@@ -7,13 +7,34 @@ function CreateEvent() {
             <div className="Card">
                 <h1>Create Your Event</h1>
                 <div className="input-group mb-3">
-                    <span className="input-group-text" id="basic-addon1">@</span>
-                    <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+                    <span className="input-group-text" id="basic-addon1">Organization Name</span>
+                    <input type="text" className="form-control" placeholder="Organization" aria-label="Organization" aria-describedby="basic-addon1" />
+
+                </div>
+
+                <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1">Event Name</span>
+                    <input type="text" className="form-control" placeholder="Event" aria-label="Event" aria-describedby="basic-addon1" />
+                </div>
+
+                <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1">Event Location</span>
+                    <input type="text" className="form-control" placeholder="Location" aria-label="Location" aria-describedby="basic-addon1" />
+                </div>
+
+                <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1">Event Date</span>
+                    <input type="text" className="form-control" placeholder="Date" aria-label="Date" aria-describedby="basic-addon1" />
+                    
+                    <span className="input-group-text">Description</span>
+                    <textarea className="form-control" aria-label="Description"></textarea>
                 </div>
 
 
-                </div>
+
+
             </div>
+        </div>
     );
 }
 
