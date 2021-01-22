72e867ad174e8fbc746e6dc0d8a302e18be47724
PJ <boyle.peter614@gmail.com>
2021-01-22 00:06:42 -0500

    updated user model and routes.

---
+++ client/src/utils/Passport/index.js
@@ -0,0 +1,28 @@
+const passport = require('passport')
+const Authenticate = require('./Passport')
+const User = require('../../../../models/user')
+
+passport.serializeUser((user, done) => {
+	console.log('SerializeUser: ')
+	console.log(user) 
+	console.log('---------')
+	done(null, { _id: user._id })
+})
+
+passport.deserializeUser((id, done) => {
+	console.log('DeserializeUser called')
+	User.findOne(
+		{ _id: id },
+		'username',
+		(err, user) => {
+			console.log('Deserialize user, user:')
+			console.log(user)
+			console.log('--------------')
+			done(null, user)
+		}
+	)
+})
+
+passport.use(Authenticate)
+
+module.exports = passport
--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const mongoose = require("mongoose");
 const routes = require("./routes")
 const PORT = process.env.PORT || 3000;
 const app = express();
+const passport = require('./passport');
 
 //Middle ware for login/ Business/Org
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 
 if(process.env.NODE_ENV == "production") {
